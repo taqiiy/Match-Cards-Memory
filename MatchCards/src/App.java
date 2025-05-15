9644f6c120a80fd33c9a274b957f9f17b20e3e42
taqiiy <taqiyataqiya3@gmail.com>
2025-05-15 11:29:34 +0700

    Add files via upload

---
+++ MatchCards/src/App.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ASUS
+ */
+public class App {
+    public static void main(String[] args) throws Exception {
+        MatchCards matchCards = new MatchCards();
+    }
+}
